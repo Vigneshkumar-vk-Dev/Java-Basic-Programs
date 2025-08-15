8d9fcb5e684522871a509f1f3a6829dbe51a5019
Vigneshkumar-vk-Dev <vigneshvikneshvk@gmail.com>
2025-08-15 17:59:42 +0530

    Create VariablesExample.java

---
+++ VariablesExample.java
@@ -0,0 +1,19 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 20;
+        double salary = 45000.50;
+        char grade = 'A';
+        boolean isPassed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+    }
+}
+/*
+Output:
+Age: 20
+Salary: 45000.5
+Grade: A
+Passed: true */
