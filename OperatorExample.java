c1e1df804b4c50d4c305023edf7b95ecfd4acd13
Vigneshkumar-vk-Dev <vigneshvikneshvk@gmail.com>
2025-08-15 18:01:47 +0530

    Create OperatorExample.java

---
+++ OperatorExample.java
@@ -0,0 +1,19 @@
+public class OperatorsExample {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulo: " + (a % b));
+    }
+}
+/*
+Outpit:
+Addition: 15
+Subtraction: 5
+Multiplication: 50
+Division: 2
+Modulo: 0
+*/
